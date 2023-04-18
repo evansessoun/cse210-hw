b66de7538310ccabfcfad7455725dba3d05e8958
evansessoun <essoun.evans@gmail.com>
2023-04-18 07:41:25 +0000

    Added codes to receive and display user names.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        /* Receives user's first name and stores it in a variable firstName*/
+        Console.Write("What is your First Namu? ");
+        string firstName = Console.ReadLine();
+
+        /* Receives user's last name and stores it in a variable lastName*/
+        Console.Write("What is your Last Name? ");
+        string lastName = Console.ReadLine();
+
+        /* Display an empty spacing followed by user's last name , full name. */
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        
     }
 }
