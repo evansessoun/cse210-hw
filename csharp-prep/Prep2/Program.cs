18f62f4cec4d10d68b1070fdf3a7d75527c02d1c
evansessoun <essoun.evans@gmail.com>
2023-04-18 20:35:17 +0000

    Changed Prep2 to a grade calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage score? ");
+        int score = int.Parse(Console.ReadLine());
+        
+        string Letter ;
+        Letter = "";
+
+        string gradeSign;
+        gradeSign = "";
+        if (score % 10 >= 7 &&  score < 90 && score >= 60)
+        {
+            gradeSign = "+";
+        }
+        else if (score % 10 <= 3  && score >= 60 )
+        {
+            gradeSign = "-";
+        }
+
+        if (score >= 90)
+        {
+            Letter = "A";
+            
+        }
+        else if (score >= 80)
+        {
+            Letter = "B";
+        }
+        else if (score >= 70)
+        {
+            Letter = "C";
+        }
+        else if (score >= 60)
+        {
+            Letter = "D";
+        }
+        else
+        {
+            Letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {Letter}{gradeSign}.");
     }
 }
